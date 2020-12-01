4a78d48727bcab8f76ebe5508ac4061e4489d6e7
Vedant Bhatt <vedantbhatt@verizonmedia.com>
2020-11-30 23:53:33 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+// App Config
+const app = express();
+const port = process.env.PORT || 8001;
+
+// Middlewares
+
+// DB Config
+
+// API Endpoints
+app.get('/', (req, res) => {
+    return res.status(200).send('Hello World');
+});
+
+// Listener
+app.listen(port, () => console.log(`listening on localhost:${port}`))
